db0fa1e2342a7ed96b27839aed378de4fd37f8cf
github username <github email address>
2019-06-19 13:54:19 -0700

    done

---
+++ longestStrings.js
@@ -0,0 +1,19 @@
+function longestStrings(arr) {
+    let lgth = 0;
+    let result = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length > lgth) {
+            lgth = arr[i].length;
+        }
+    } 
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j].length == lgth) {
+            result.push(arr[j])
+        }
+    }
+  
+    return result
+}
+
+console.log(longestStrings(['blah', 'stuff', "at"]))
